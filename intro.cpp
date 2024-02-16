574eb5f21ccddb43b0718b8fac74e4a704f4669f
Noor e Azeem <nooreazeemsiddiqui99@gmail.com>
2024-02-17 01:21:03 +0500

    Intro to C++

---
+++ intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int since = 1979;
+    cout<<"The Palestine is illegaly occupied by oppressor Israel since "<<since;
+    return 0;
+}
+
+
