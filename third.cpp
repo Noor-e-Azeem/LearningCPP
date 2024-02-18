dc2accaca5195f9a4d88d7c33b746fddf8b1b5a0
Noor e Azeem <nooreazeemsiddiqui99@gmail.com>
2024-02-18 22:52:05 +0500

    Scope Res. Operator, Literals, Reference Var & Typecasting

--- third.cpp
+++ third.cpp
@@ -14,7 +14,39 @@ int main()
     int c = a + b; // Here, c as local variable
 
     cout << "The sum is of a and b is " << c << endl;
-    cout << "Here we want value of global c, while the priority is given to local c, so here we use scope resolution operator :: to access global c in block: " << ::c;
+    cout << "Here we want value of global c, while the priority is given to local c, so here we use scope resolution operator :: to access global c in block: " << ::c << endl;
+
+    // ******************************************************************************* //
+
+    // Literals --> Assigning type of var by putting f or F and l or L is important for writing programs as they have different size of memories
+
+    float x = 3.14f;
+    long double y = 3.14l;
+
+    cout << sizeof(3.14) << endl;
+    cout << sizeof(3.14f) << endl;
+    cout << sizeof(3.14F) << endl;
+    cout << sizeof(3.14l) << endl;
+    cout << sizeof(3.14L) << endl;
+
+    // ***********************Reference Variables************************** //
+
+    // Single variable, different names, just referencing
+
+    int p = 43;
+    int &q = p;
+    cout << p << endl;
+    cout << q << endl;
+
+    // ***********************Typecasting************************** //
+
+    // Its simply, changing variable type
+
+    float r = 45.46;
+
+    cout << "The value of b is " << (int)r << endl;
+    cout << "The value of b is " << int(r) << endl;
+    int c = int(r);
 
     return 0;
 }
