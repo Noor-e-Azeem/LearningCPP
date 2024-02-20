a6c6b6d0516ee2d78a1761e646b15acce16e5c9f
Noor e Azeem <nooreazeemsiddiqui99@gmail.com>
2024-02-21 02:12:29 +0500

    Selection Control Structure

---
+++ forth.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /***********************  C++ Control Structures *************************/ 
+
+    /********** 1) Sequence 2) Selection 3) Loop Control Structures **********/
+
+    /*** Selection Control Structure ***/
+
+    // If-Else Ladder
+
+    int age;
+    cout << "Tell me your age" << endl;
+    cin >> age;
+
+    if (age < 12 && age > 0)
+    {
+        cout << "You are kid.";
+    }
+    else if (age > 12 && age < 18)
+    {
+        cout << "You are teenager";
+    }
+    else if (age > 18)
+    {
+        cout << "You are an adult";
+    }
+
+    return 0;
+}
